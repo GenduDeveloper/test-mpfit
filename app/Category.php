e54920247be1edc67497bbe2a8677051ffcf63ba
Ilya Avkhimenko <avximenko@inbox.ru>
2025-05-21 12:45:40 +0800

    T-002: Создал миграцию, модель и сид для таблицы categories

---
+++ app/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = 'categories';
+
+    public $timestamps = false;
+
+    protected $fillable = ['name'];
+}
