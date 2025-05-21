db0762d5d91f45d2f61a3a1af2632ef7412ac47e
Ilya Avkhimenko <avximenko@inbox.ru>
2025-05-21 13:46:53 +0800

    T-002: Создал миграцию, модель, фабрику и сид для таблицы products. Описал отношение между моделями Product и Category.

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends Factory<\App\Category>
+ * @extends Factory<\App\Models\Category>
  */
 class CategoryFactory extends Factory
 {
